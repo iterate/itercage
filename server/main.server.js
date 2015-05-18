fc5364f47b7863acfc3fcd2dd82fb1ef01bbd0b4
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2015-05-18 18:14:37 +0200

    Routing, email, new way of clearing list

---
+++ server/main.server.js
@@ -0,0 +1,30 @@
+ServerConfig = {
+  SUPERSECRET_PASSWORD: "trulsersjef1337",
+  OWNER_EMAIL_ADDRESS: "henrikgs@iterate.no",
+  FROM_EMAIL_ADDRESS: "fotball@iterate.no"
+}
+
+var informOwnerOfNewAttendee = function (newAttendee) {
+  var numberOfAttendees = Attendees.find({}).count();
+
+  Email.send({
+    to: ServerConfig.OWNER_EMAIL_ADDRESS,
+    from: ServerConfig.FROM_EMAIL_ADDRESS,
+    subject: "[itercage] " + numberOfAttendees + " påmeldte",
+    text: newAttendee.name + " melte seg på.\n\nDet er nå " + numberOfAttendees + " påmeldte"
+  });
+}
+
+Meteor.startup(function () {
+
+  Attendees.find({}).observe({
+    added: function (attendee) {
+      if (attendee.name === ServerConfig.SUPERSECRET_PASSWORD) {
+        Attendees.remove({});
+      }
+
+      informOwnerOfNewAttendee(attendee);
+    }
+  });
+
+});
